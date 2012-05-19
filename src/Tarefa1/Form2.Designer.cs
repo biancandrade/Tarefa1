54f4122770a04afef6deaedc29939cdd705f5892
Bianca Andrade <bianca_porfirio@yahoo.com.br>
2012-05-19 16:14:20 -0300

    Criacao do formulario para calcular ano bissexto

--- src/Tarefa1/Form2.Designer.cs
+++ src/Tarefa1/Form2.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Tarefa1
 {
-    partial class Form1
+    partial class Form2
     {
         /// <summary>
         /// Required designer variable.
@@ -29,38 +29,46 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
-            this.button1.Location = new System.Drawing.Point(27, 72);
+            this.button1.Location = new System.Drawing.Point(94, 90);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(99, 40);
+            this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
             this.button1.Text = "Clique Aqui";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(94, 29);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
-            this.label1.Location = new System.Drawing.Point(179, 74);
+            this.label1.Location = new System.Drawing.Point(12, 32);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(0, 17);
-            this.label1.TabIndex = 1;
+            this.label1.Size = new System.Drawing.Size(75, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Digite um ano:";
             // 
-            // Form1
+            // Form2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(457, 229);
+            this.ClientSize = new System.Drawing.Size(294, 139);
             this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
-            this.Name = "Form1";
-            this.Text = "Lista de números";
+            this.Name = "Form2";
+            this.Text = "Form2";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -69,7 +77,7 @@
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Label label1;
     }
 }
-
---
+++ src/Tarefa1/Form2.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Tarefa1
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            CalcularAnoBissexto();
+        }
+
+        public void CalcularAnoBissexto()
+        {
+            var ano = Convert.ToInt32(textBox1.Text);
+
+            if (ano % 4 == 0 && ano % 400 == 0)
+                MessageBox.Show("É ano Bisssexto", "Mensagem", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            else
+                MessageBox.Show("Não é ano Bisssexto", "Mensagem", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
